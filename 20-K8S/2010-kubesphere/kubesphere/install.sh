5a0fb6a5bc292197d2eb19ad56753c13946b5cdd
thinkbase <thinkbase.net@gmail.com>
2021-09-25 15:02:32 +0800

    feature: one master - two nodes cluster of kubeshpere

---
+++ 20-K8S/2010-kubesphere/kubesphere/install.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+set -o nounset
+set -o errexit
+
+echo "[$(date +%Y%m%d-%H%M%S)] >>> Run '$0' at '$(hostname)', with account '$(id)' ..." >&2
+
+if [ "root" != "$(id -ng)" ]; then
+	echo "MUST run '$0' as root, account '$(id)' invalid!"
+	exit -1
+fi
+
+mkdir -pv /vagrant/kubesphere/.work
+pushd /vagrant/kubesphere/.work
+
+export KKZONE=cn
+
+curl -sfL https://get-kk.kubesphere.io | VERSION=v1.1.1 sh -
+chmod +x kk
+
+_START="$(date +%Y%m%d-%H%M%S)"
+
+echo "yes" | ./kk create cluster -f ../config-vagrant.yaml -
+
+echo "[$(date +%Y%m%d-%H%M%S)] >>> Run '$0' success, from [$_START]."
+
+popd
