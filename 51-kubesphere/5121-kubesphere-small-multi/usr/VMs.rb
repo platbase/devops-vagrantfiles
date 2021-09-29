218d7eafbc6912c1ace9987297d9e8985fe67d74
thinkbase <thinkbase.net@gmail.com>
2021-09-29 17:56:19 +0800

    feature: Memory optimization for small multi-nodes cluster - IF worked as both master and node, the minimum amount of memory is 6G

--- 51-kubesphere/5121-kubesphere-small-multi/usr/VMs.rb
+++ 51-kubesphere/5121-kubesphere-small-multi/usr/VMs.rb
@@ -1,13 +1,13 @@
 VMs = {
     main1: {
         :eth1 => "10.1.1.11",
-        :cpu => "2", :cpu_cap => 0.5,
-        :mem => "4096",
+        :cpu => "4", :cpu_cap => 0.5,
+        :mem => "6144",  # IF worked as both master and node, the minimum amount of memory is 6G
         :ports => { 30880 => 30880 }
     },
     node1: {
         :eth1 => "10.1.1.21",
         :cpu => "2", :cpu_cap => 0.33,
-        :mem => "4096"
+        :mem => "3072"
     }
 }
